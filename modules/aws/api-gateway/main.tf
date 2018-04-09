34535d72b6b339f790f3c7e50af9fbdd25e9b2d4
Alex Tan Hong Pin <alextan@seekasia.com>
2018-04-09 21:54:20 +0800

    chore: add suggestion for proxy

--- modules/aws/api-gateway/main.tf
+++ modules/aws/api-gateway/main.tf
@@ -17,6 +17,7 @@ resource "aws_api_gateway_rest_api" "app" {
 resource "aws_api_gateway_resource" "app" {
 	rest_api_id = "${aws_api_gateway_rest_api.app.id}"
 	parent_id = "${aws_api_gateway_rest_api.app.root_resource_id}"
+	# TODO: Test this which will enable proxy: "{proxy+}"
 	path_part = "${var.path_part}"
 }
 
